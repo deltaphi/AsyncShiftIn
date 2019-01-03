3cb1e348890d8a2967e26c89a7d767dfe7fcf3b1
Damian Philipp <damian.philipp@gmx.net>
2019-01-03 20:00:00 +0100

    Update comments and formatting

--- AsyncShiftIn.cpp
+++ AsyncShiftIn.cpp
@@ -2,7 +2,7 @@
  * Copyright 2013 Damian Philipp
  *
  * This file is part of "AsyncShiftIn".
- * 
+ *
  * "AsyncShiftIn" is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -18,15 +18,15 @@
  *
  * Diese Datei ist Teil von "AsyncShiftIn".
  *
- * "AsyncShiftIn" ist Freie Software: Sie können es unter den Bedingungen
+ * "AsyncShiftIn" ist Freie Software: Sie kï¿½nnen es unter den Bedingungen
  * der GNU General Public License, wie von der Free Software Foundation,
- * Version 3 der Lizenz oder (nach Ihrer Option) jeder späteren
- * veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+ * Version 3 der Lizenz oder (nach Ihrer Option) jeder spï¿½teren
+ * verï¿½ffentlichten Version, weiterverbreiten und/oder modifizieren.
  *
- * "AsyncShiftIn" wird in der Hoffnung, dass es nützlich sein wird, aber
- * OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
- * Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
- * Siehe die GNU General Public License für weitere Details.
+ * "AsyncShiftIn" wird in der Hoffnung, dass es nï¿½tzlich sein wird, aber
+ * OHNE JEDE GEWï¿½HRLEISTUNG, bereitgestellt; sogar ohne die implizite
+ * Gewï¿½hrleistung der MARKTFï¿½HIGKEIT oder EIGNUNG Fï¿½R EINEN BESTIMMTEN ZWECK.
+ * Siehe die GNU General Public License fï¿½r weitere Details.
  *
  * Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
  * Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
@@ -35,22 +35,24 @@
 #include <Arduino.h>
 #include <AsyncShiftIn.h>
 
-void AsyncShiftIn::init(uint8_t data, uint8_t clock, uint8_t load, uint8_t reset, unsigned int length, unsigned int clkPeriod, unsigned int resetPeriod) {
+void AsyncShiftIn::init(uint8_t data, uint8_t clock, uint8_t load,
+                        uint8_t reset, unsigned int length,
+                        unsigned int clkPeriod, unsigned int resetPeriod) {
   // Init the hardware
-  this->data = data; 
-  this->clock = clock; 
+  this->data = data;
+  this->clock = clock;
   this->load = load;
   this->reset = reset;
   pinMode(this->clock, OUTPUT);
-  pinMode(this->load, OUTPUT); 
+  pinMode(this->load, OUTPUT);
   pinMode(this->reset, OUTPUT);
   pinMode(this->data, INPUT);
   digitalWrite(this->clock, LOW);
   digitalWrite(this->load, LOW);
-  digitalWrite(this->reset, LOW); 
-  
-  this->length = length; 
-  this->nextBit = length; // act as if we were at the end
+  digitalWrite(this->reset, LOW);
+
+  this->length = length;
+  this->nextBit = length;  // act as if we were at the end
   this->state = READ_SAMPLE;
   this->clkPeriod = clkPeriod;
   this->resetPeriod = resetPeriod;
@@ -60,35 +62,36 @@ void AsyncShiftIn::init(uint8_t data, uint8_t clock, uint8_t load, uint8_t reset
 
 void AsyncShiftIn::loop() {
   // check for activity
-  unsigned long sleepDuration(micros() - lastActiveTime); // this avoids the overflow problem
+  unsigned long sleepDuration(
+      micros() - lastActiveTime);  // this avoids the overflow problem
   if (sleepDuration <= currentSleepDuration) {
     return;
   }
-  
+
   // enter new state
   stateTransition();
-  
+
   // Sample
   int readValue = 0;
   if (state == READ_SAMPLE || state == RES6_SAMPLE) {
     readValue = digitalRead(data);
   }
-  
+
   // configure output
   setupOutput();
-  
+
   // setup sleep duration
   setupSleepDuration();
-  
+
   // setup lastActiveTime
   lastActiveTime = micros();
-  
+
   // run callbacks
   if (AsyncShiftIn_shift && (state == READ_SAMPLE || state == RES6_SAMPLE)) {
     AsyncShiftIn_shift(this, nextBit, readValue);
     ++nextBit;
   }
-  
+
   if (AsyncShiftIn_reset && state == RES1) {
     AsyncShiftIn_reset(this);
   }
@@ -98,10 +101,10 @@ void AsyncShiftIn::stateTransition() {
   switch (state) {
     case READ_HIGH:
       state = READ_LOW;
-    break;
+      break;
     case READ_LOW:
       state = READ_SAMPLE;
-    break;
+      break;
     case READ_SAMPLE:
       if (nextBit < length) {
         state = READ_HIGH;
@@ -112,60 +115,60 @@ void AsyncShiftIn::stateTransition() {
       break;
     case RES1:
       state = RES2;
-    break;
+      break;
     case RES2:
       state = RES3;
-    break;
+      break;
     case RES3:
       state = RES4;
-    break;
+      break;
     case RES4:
       state = RES5;
-    break;
+      break;
     case RES5:
       state = RES6;
-    break;
+      break;
     case RES6:
       state = RES6_SAMPLE;
-    break;
+      break;
     case RES6_SAMPLE:
       state = READ_HIGH;
-    break;
+      break;
   }
 }
 
 void AsyncShiftIn::setupSleepDuration() {
-    switch (state) {
+  switch (state) {
     case READ_HIGH:
       currentSleepDuration = clkPeriod;
-    break;
+      break;
     case READ_LOW:
       currentSleepDuration = clkPeriod / 2;
-    break;
+      break;
     case READ_SAMPLE:
       currentSleepDuration = clkPeriod / 2;
       break;
     case RES1:
       currentSleepDuration = resetPeriod;
-    break;
+      break;
     case RES2:
       currentSleepDuration = resetPeriod;
-    break;
+      break;
     case RES3:
       currentSleepDuration = resetPeriod;
-    break;
+      break;
     case RES4:
       currentSleepDuration = resetPeriod;
-    break;
+      break;
     case RES5:
       currentSleepDuration = resetPeriod;
-    break;
+      break;
     case RES6:
       currentSleepDuration = resetPeriod / 2;
-    break;
+      break;
     case RES6_SAMPLE:
       currentSleepDuration = resetPeriod / 2;
-    break;
+      break;
   }
 }
 
@@ -173,32 +176,32 @@ void AsyncShiftIn::setupOutput() {
   switch (state) {
     case READ_HIGH:
       digitalWrite(clock, HIGH);
-    break;
+      break;
     case READ_LOW:
       digitalWrite(clock, LOW);
-    break;
+      break;
     case READ_SAMPLE:
       break;
     case RES1:
       digitalWrite(clock, LOW);
       digitalWrite(load, HIGH);
-    break;
+      break;
     case RES2:
       digitalWrite(clock, HIGH);
-    break;
+      break;
     case RES3:
       digitalWrite(clock, LOW);
-    break;
+      break;
     case RES4:
       digitalWrite(reset, HIGH);
-    break;
+      break;
     case RES5:
       digitalWrite(reset, LOW);
-    break;
+      break;
     case RES6:
       digitalWrite(load, LOW);
-    break;
+      break;
     case RES6_SAMPLE:
-    break;
+      break;
   }
 }
--- AsyncShiftIn.h
+++ AsyncShiftIn.h
@@ -2,7 +2,7 @@
  * Copyright 2013 Damian Philipp
  *
  * This file is part of "AsyncShiftIn".
- * 
+ *
  * "AsyncShiftIn" is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -18,15 +18,15 @@
  *
  * Diese Datei ist Teil von "AsyncShiftIn".
  *
- * "AsyncShiftIn" ist Freie Software: Sie können es unter den Bedingungen
+ * "AsyncShiftIn" ist Freie Software: Sie kï¿½nnen es unter den Bedingungen
  * der GNU General Public License, wie von der Free Software Foundation,
- * Version 3 der Lizenz oder (nach Ihrer Option) jeder späteren
- * veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+ * Version 3 der Lizenz oder (nach Ihrer Option) jeder spï¿½teren
+ * verï¿½ffentlichten Version, weiterverbreiten und/oder modifizieren.
  *
- * "AsyncShiftIn" wird in der Hoffnung, dass es nützlich sein wird, aber
- * OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
- * Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
- * Siehe die GNU General Public License für weitere Details.
+ * "AsyncShiftIn" wird in der Hoffnung, dass es nï¿½tzlich sein wird, aber
+ * OHNE JEDE GEWï¿½HRLEISTUNG, bereitgestellt; sogar ohne die implizite
+ * Gewï¿½hrleistung der MARKTFï¿½HIGKEIT oder EIGNUNG Fï¿½R EINEN BESTIMMTEN ZWECK.
+ * Siehe die GNU General Public License fï¿½r weitere Details.
  *
  * Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
  * Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
@@ -37,57 +37,100 @@
 
 #include <Arduino.h>
 
-class AsyncShiftIn {
-  protected:
-  // Pinout configuration
-    uint8_t data;
-    uint8_t clock;
-    uint8_t load;
-    uint8_t reset;
-    
-    // Internal variables
-    unsigned int length;
-    unsigned int nextBit;
-    enum State { RES1, RES2, RES3, RES4, RES5, RES6, RES6_SAMPLE, READ_HIGH, READ_LOW, READ_SAMPLE };
-    State state;
-    void setupOutput();
-    
-    unsigned int clkPeriod;
-    unsigned int resetPeriod;
-    unsigned long currentSleepDuration;
-    unsigned long lastActiveTime;
-    void setupSleepDuration();
-    void stateTransition();
+class AsyncShiftIn
+{
+public:
+  using PinIndexType = uint8_t;
+  using ClockTimeType = unsigned int;
+  using SleepTimeType = unsigned long;
 
-  public:
-    // Initializes this shift register.
-	// data: Input Pin for data from the shift register
-	// clock: Output Pin for generating the clock signal
-	// load: Output Pin for generating the load signal
-	// reset: Output Pin for generating the reset signal
-	// length: The number of bits to shift in before resetting the shift register
-	// clkPeriod: The duration of the clock period in the read phase (in MICROseconds!)
-    // resetPeriod: The duration of the clock period in the reset phase (in MICROseconds!)
-    void init(uint8_t data, uint8_t clock, uint8_t load, uint8_t reset, unsigned int length, unsigned int clkPeriod, unsigned int resetPeriod);
-    
-	// Call loop() in your main loop. When the time has come, this will update the pins and call the appropriate callbacks.
-	void loop();
+protected:
+  // Pinout configuration
+  PinIndexType data;
+  PinIndexType clock;
+  PinIndexType load;
+  PinIndexType reset;
+
+  // Internal variables
+  unsigned int length;
+  unsigned int nextBit;
+  enum State
+  {
+    RES1,
+    RES2,
+    RES3,
+    RES4,
+    RES5,
+    RES6,
+    RES6_SAMPLE,
+    READ_HIGH,
+    READ_LOW,
+    READ_SAMPLE
+  };
+  State state;
+
+  ClockTimeType clkPeriod;
+  ClockTimeType resetPeriod;
+  TimeType currentSleepDuration;
+  TimeType lastActiveTime;
+
+  void setupOutput();
+  void setupSleepDuration();
+  void stateTransition();
+
+public:
+  /**
+   * \brief Initializes this shift register.
+   *
+   * \param data Input Pin for data from the shift register
+   * \param clock Output Pin for generating the clock signal
+   * \param load Output Pin for generating the load signal
+   * \param reset Output Pin for generating the reset signal
+   * \param length The number of bits to shift in before resetting the shift register
+   * \param clkPeriod The duration of the clock period in the read phase (in MICROseconds!)
+   * \param resetPeriod The duration of the clock period in the reset phase (in MICROseconds!)
+   */
+  void init(uint8_t data, uint8_t clock, uint8_t load, uint8_t reset,
+            unsigned int length, unsigned int clkPeriod,
+            unsigned int resetPeriod);
+
+  /**
+   * \brief Main Loop to update pins and trigger callbacks.
+   *
+   * Call loop() in your main loop. When the time has come, this will update the
+   * pins and call the appropriate callbacks.
+   */
+  void loop();
 };
 
-// This callback is called whenever a new bit has been shifted in.
-// asyncShiftIn: A pointer to the calling AsyncShiftIn object to distinguish between multiple shift registers
-// bitNumer: The index of the bit that was just shifted in (0-based)
-// state: The state of the bit that was just shifted in (LOW/HIGH)
-//
-// Please make sure to keep the processing in this method fast, so that the timing won't suffer too much.
-//
-extern void AsyncShiftIn_shift(const AsyncShiftIn * asyncShiftIn, unsigned int bitNumber, uint8_t state) __attribute__ ((weak));
+/**
+ * \brief This callback is called whenever a new bit has been shifted in.
+ *
+ * Please make sure to keep the processing in this method fast, so that the
+ * timing won't suffer too much. If you have no processing to do for individual
+ * bits (unlikely...), you can omit this callback.
+ *
+ * \param asyncShiftIn A pointer to the calling AsyncShiftIn object to
+ * distinguish between multiple shift registers
+ * \param bitNumer The index of the bit that was just shifted in (0-based)
+ * \param state The state of the bit that was just shifted in (LOW/HIGH)
+ */
+extern void AsyncShiftIn_shift(const AsyncShiftIn *asyncShiftIn,
+                               unsigned int bitNumber, uint8_t state)
+    __attribute__((weak));
 
-// This callback is called whenever the shift register has been reset, i.e., it indicates that a read cycle was completed.
-// asyncShiftIn: A pointer to the calling AsyncShiftIn object to distinguish between multiple shift registers
-//
-// Please make sure to keep the processing in this method fast, so that the timing won't suffer too much.
-//
-extern void AsyncShiftIn_reset(const AsyncShiftIn * asyncShiftIn) __attribute__ ((weak));
+/**
+ * \brief This callback is called whenever the shift register has been reset.
+ *
+ * This callback indicates that a read cycle was completed. Please make sure to
+ * keep the processing in this method fast, so that the timing won't suffer too
+ * much.
+ * If you have no processing to do per cycle, you can omit this callback.
+ *
+ * \param asyncShiftIn A pointer to the calling AsyncShiftIn object to
+ * distinguish between multiple shift registers
+ */
+extern void AsyncShiftIn_reset(const AsyncShiftIn *asyncShiftIn)
+    __attribute__((weak));
 
 #endif
